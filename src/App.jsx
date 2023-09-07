00182dfbc788e88a5208c0b0cfb10808a3e3144b
Aman-201 <81082654+Aman-201@users.noreply.github.com>
2023-09-07 20:04:10 +0530

    amount count implemented

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import CurrencyBox from './Components/CurrencyBox'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [amount, setAmount] = useState(0)
+  const [currency1,setCurrency1]=useState('')
+  const [currency2,setCurrency2]=useState('') 
   return (
     <>
     <div className='backdrop-blur-3xl	mx-5 my-5 px-5 py-5 bg-white/30 flex flex-col static '>
-      <CurrencyBox className="  " />
+      <CurrencyBox className="  "  amount={amount} setAmount={setAmount} amt_flag={true} />
       <button className='z-1 top-24  bottom-22 right-1/2 left-auto absolute m-auto bg-blue-500 px-0 py-0'>swap</button>
-      <CurrencyBox  className=" " />
+      <CurrencyBox  className=" " which="to" />
     </div>
     </>
   )
--- src/Components/CurrencyBox.jsx
+++ src/Components/CurrencyBox.jsx
@@ -1,20 +1,30 @@
-function CurrencyBox({amount=0,currency="America"})
+function CurrencyBox({amount=0,setAmount,currency="America",amt_flag=false,which="from"})
 {
     return (
        <div className="bg-white flex my-3">
         <div className="px-4 ">
-            <p className=" my-0">From</p>
+            <p className=" my-0">{which}</p>
             <p className=" py-2"></p>
             <p className="">{amount}</p>
         </div>
-        <div className="px-4 my-auto">
-            <button>+</button>
-            <button>-</button>
+        {(amt_flag==true)? <div className="px-4 my-auto">
+            <button onClick={()=>{setAmount(amount+1)}}>+</button>
+            <button onClick={()=>{if(amount>0)setAmount(amount-1)}}>-</button>
+        </div>:<div className=" mx-auto my-auto"></div>}
+        {/* {(amt_flag==true)
+        {
+            <div className="px-4 my-auto">
+            <button onClick={()=>{setAmount(amount+1)}}>+</button>
+            <button onClick={()=>{setAmount(amount+1)}}>-</button>
         </div>
+
+        }} */}
+      
         <div className="px-4 ">
-            <p>Currency Type</p>
+            <p >Currency Type</p>
             <p className=" py-2"></p>
-            <p>{currency}</p>
+            {/* <label htmlFor="country">{currency}</label> */}
+            <input type="dropdown" id="country" value={currency} className="mx-auto w-20"/>
         </div>
        </div>
     )
