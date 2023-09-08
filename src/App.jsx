3267503ff40266ee999ae566c66b04f45ca7f98d
Aman-201 <81082654+Aman-201@users.noreply.github.com>
2023-09-08 23:57:42 +0530

    added api code

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,10 @@ function App() {
   const [amount, setAmount] = useState(0)
   const [currency1,setCurrency1]=useState('')
   const [currency2,setCurrency2]=useState('') 
+  let currency=[];
+  fetch('https://api.freecurrencyapi.com/v1/currencies?apikey=fca_live_9TVN51XSRp7W5qMHiklAYXycAc3HdBCNwU15Zk1k&currencies=')
+   .then(response => response.json())
+   .then((data) =>{console.log(data); Object.keys(data).forEach((key)=>{currency.push(data[key])}); console.log("----");console.log(currency.length) });
   return (
     <>
     <div className='backdrop-blur-3xl	mx-5 my-5 px-5 py-5 bg-white/30 flex flex-col static '>
